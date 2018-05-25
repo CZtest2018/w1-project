bb913f4d357b7aa25b2d4f41bf21c6d476f13ca9
Chris Zuraff <cmzuraff@yahoo.com>
2018-05-25 16:23:55 -0700

    Friday push

---
+++ finalProject/w1.js
@@ -0,0 +1,11 @@
+function idCard(){
+    var firstName= document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    doument.getElementById('postFullName').innerHTML = firstName +''+ lastName;
+    var address= document.getElementById("address").value;
+    document.getElementById("postAddress").value;
+
+
+
+}
+
