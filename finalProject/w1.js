4ecb5edf6ec41bd518c3768a3827f9efb03975d2
ChristopherZuraff <36679712+ChristopherZuraff@users.noreply.github.com>
2018-05-28 15:38:01 -0700

    Sunday afternoon push

--- finalProject/w1.js
+++ finalProject/w1.js
@@ -1,11 +1,26 @@
-function idCard(){
+ function idCard(){
+   
+   //part1
     var firstName= document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
-    doument.getElementById('postFullName').innerHTML = firstName +''+ lastName;
+     var postFullName = firstName +''+ lastName;
+     document.getElementById("postFullName").innerHTML = postFullName;
     var address= document.getElementById("address").value;
-    document.getElementById("postAddress").value;
+    document.getElementById("postAddress").innerHTML= address;
+   
+   
+   //part2
+    var age = document.getElementById("Age").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
 
+    var numberArray = [age, phoneNumber];
 
+    for (var i=0, i<=100, i++)
+    {
+
+    }
+
+//document.getElementById("button").addEventListener("click", idCard);
 
 }
 
